65216629f7217a5899a05d9827ea50075325eb99
magdalenarussell <magdalenarussell23@gmail.com>
2021-02-22 08:05:24 -0800

    constructing PWMs matrix method

--- scripts/plot_functions.R
+++ scripts/plot_functions.R
@@ -1,25 +1,26 @@
-get_PFM <- function(group_name, trim_lengths_list){
+get_PFM <- function(group_name, weighting = NULL){
     output_path = file.path('_ignore', 'pfms', 'complete') 
-    file = file.path(output_path, paste0(group_name, '_', PFM_TYPE, '_', MOTIF_TYPE, '_', paste(trim_lengths_list, collapse = ''), '.tsv'))
+    file = file.path(output_path, paste0(group_name, '_', PFM_TYPE, '_', MOTIF_TYPE, '_', weighting, '.tsv'))
     PFM = as.matrix(fread(file), rownames = 1)
     return(PFM)
 }
 
 
-create_plot_title <- function(group_name){
+create_plot_title <- function(group_name, weighting = NULL){
     if (group_name == 'ALL'){
         title = paste0(PFM_TYPE, ' ', MOTIF_TYPE, ' for ', group_name, ' subjects')
     } else{
         snp = strsplit(group_name, '_')[[1]][1]
         title = paste0(PFM_TYPE, ' ', MOTIF_TYPE, ' by genotype for SNP ', snp)
     }
+    title = ifelse(is.null(weighting), title, paste0(title, ' with gene/nt weighting'))
     return(title)
 }
 
-create_file_name <- function(group_name, trim_lengths_list, plot_type){
+create_file_name <- function(group_name, weighting = NULL, plot_type){
     output_path = file.path('plots') 
     dir.create(output_path, recursive = TRUE, showWarnings = FALSE)
-    filename = file.path(output_path, paste0(group_name, '_', PFM_TYPE, '_', MOTIF_TYPE, '_', paste(trim_lengths_list, collapse = ''), '_', plot_type, '.pdf'))
+    filename = file.path(output_path, paste0(group_name, '_', PFM_TYPE, '_', MOTIF_TYPE, '_', weighting, '_', plot_type, '.pdf'))
     return(filename)
 }
 
@@ -31,16 +32,18 @@ transform_PWM_to_dataframe <- function(PWM){
     return(df)
 }
 
-plot_single_PWM_heatmap <- function(group_name, trim_lengths_list, subtitle = NULL){
-    PFM = get_PFM(group_name, trim_lengths_list)
-    plot_title = create_plot_title(group_name)
-    filename = create_file_name(group_name, trim_lengths_list, 'PWM_heatmap_with_background_correction')
+plot_single_PWM_heatmap <- function(group_name, weighting = NULL, subtitle = NULL){
+    PFM = get_PFM(group_name, weighting)
+    plot_title = create_plot_title(group_name, weighting)
+    filename = create_file_name(group_name, weighting, 'PWM_heatmap_with_background_correction')
     
-    PWM = calculate_PWM_from_PFM(PFM)
+    PWM = calculate_PWM_from_PFM(PFM, weighting)/log(10)
     PWM_df = transform_PWM_to_dataframe(PWM)
+    
+    PWM_df$Base = factor(PWM_df$Base, levels = c('T', 'G', 'C', 'A'))
     PWM_df$Position = factor(PWM_df$Position, levels = sort(unique(as.numeric(PWM_df$Position))))
+
     PWM_df %>%
-        arrange(Base) %>%
         ggplot(aes(x=Position, y=Base, fill=weight)) + 
         geom_tile() +
         theme_classic() +
@@ -53,19 +56,21 @@ plot_single_PWM_heatmap <- function(group_name, trim_lengths_list, subtitle = NU
     ggsave(filename, plot = last_plot(), width = 10, height = 5, units = 'in', dpi = 750, device = 'pdf')
 }
 
-plot_PWM_heatmap_by_genotype <- function(snpID, trim_lengths_list, subtitle = NULL){
+plot_PWM_heatmap_by_genotype <- function(snpID, weighting = NULL, subtitle = NULL){
     together = data.frame()
     for (genotype in c(0,1,2)){
-        assign(paste0('PFM', genotype), get_PFM(group_name = get_group_name(snpID, genotype) , trim_lengths_list))
-        assign(paste0('PWM', genotype), calculate_PWM_from_PFM(get(paste0('PFM', genotype))))
+        assign(paste0('PFM', genotype), get_PFM(group_name = get_group_name(snpID, genotype) , weighting))
+        assign(paste0('PWM', genotype), calculate_PWM_from_PFM(get(paste0('PFM', genotype)), weighting)/log(10))
         PWM_df = transform_PWM_to_dataframe(get(paste0('PWM', genotype)))
         PWM_df$genotype = genotype
         together = rbind(together, PWM_df)
     }
 
-    plot_title = create_plot_title(group_name = paste(snpID))
+    plot_title = create_plot_title(group_name = paste(snpID), weighting)
     
-    filename = create_file_name(group_name = paste(snpID), trim_lengths_list, 'PWM_heatmap_by_GENOTYPE_with_background_correction')
+    filename = create_file_name(group_name = paste(snpID), weighting, 'PWM_heatmap_by_GENOTYPE_with_background_correction')
+
+    together$Base = factor(together$Base, levels = c('T', 'G', 'C', 'A'))
     together$Position = factor(together$Position, levels = sort(unique(as.numeric(together$Position))))
 
     together %>%
