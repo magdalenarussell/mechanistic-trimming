c9042d6a2ae6ee7211f48f3bc2e4d8c8853b2598
magdalenarussell <magdalenarussell23@gmail.com>
2021-03-08 13:24:46 -0800

    updating changes

--- scripts/pfm_class_functions/unbounded.R
+++ scripts/pfm_class_functions/unbounded.R
@@ -1,23 +1,31 @@
-subset_data_by_trim_length_bound <- function(trim_length, tcr_dataframe, cdr3_variable){
-    tcr_dataframe_subset = tcr_dataframe[get(TRIM_TYPE) == trim_length]
-    temp_subset_columns = c(GENE_NAME, 'sequences', cdr3_variable, TRIM_TYPE)
-    temp_subset = tcr_dataframe_subset[,.N,by = temp_subset_columns]
-    return(temp_subset)
-}
-
-create_PFM_by_trim_length <- function(trim_length, tcr_dataframe){
-    cdr3_variable = paste0('cdr3_nucseq_from_', substring(TRIM_TYPE, 1, 1))
-    temp_subset = subset_data_by_trim_length_bound(trim_length, tcr_dataframe, cdr3_variable)
-    motif_vector = c()
-    for (row in seq(1:nrow(temp_subset))){
-        motif = get_motif_context(whole_gene_seq = temp_subset[row,]$sequences, 
-                                  trimmed_gene_seq = temp_subset[row,][[cdr3_variable]], 
-                                  trim_length)
-        motif_vector = c(motif_vector, rep(toString(motif), temp_subset[row,]$N))
+get_unobserved_motifs <- function(tcr_dataframe){
+    tcr_dataframe_observed = tcr_dataframe[,.N, by = .(gene, trim_length)]
+    unobserved_df = data.frame()
+    for (gene_name in unique(tcr_dataframe_observed$gene)){
+        observed = unique(tcr_dataframe_observed[gene == paste(gene_name)]$trim_length)
+        unobserved = setdiff(seq(1,20), observed)
+        unobserved_df = rbind(unobserved_df, data.frame(gene = rep(gene_name, length(unobserved)), trim_length = unobserved))
     }
-    motif_vector_DNAString = DNAStringSet(motif_vector)
-    PFM = consensusMatrix(motif_vector_DNAString,baseOnly=TRUE)
-    return(PFM)
+    cols = c('whole_seq', 'gene')
+    tcr_dataframe = unique(tcr_dataframe[,..cols])
+    together = as.data.table(merge(unobserved_df, tcr_dataframe, by = 'gene'))
+    together = together[, motif:= get_motif_context_unobserved(whole_seq, trim_length)] 
+    together$observed = FALSE
+    return(together[,-c('whole_seq')])
 }
 
+get_motifs <- function(tcr_dataframe, subject_id){
+    tcr_dataframe = tcr_dataframe[get(TRIM_TYPE) > 0]
+    cdr3_variable = paste0('cdr3_nucseq_from_', substring(TRIM_TYPE, 1, 1))
+    cols = c(paste(cdr3_variable), 'sequences', paste(GENE_NAME), paste(TRIM_TYPE))
+    tcr_dataframe = tcr_dataframe[,..cols]
+    colnames(tcr_dataframe) = c('trimmed_seq', 'whole_seq', 'gene', 'trim_length')
+    tcr_dataframe = tcr_dataframe[,motif:=get_motif_context(whole_seq, trimmed_seq, trim_length)]
 
+    tcr_dataframe$observed = TRUE
+    unobserved = get_unobserved_motifs(tcr_dataframe)
+    together = rbind(tcr_dataframe[,-c(1,2)], unobserved)
+    together$gene_type = GENE_NAME
+    together$subject = subject_id
+    return(together)
+}
