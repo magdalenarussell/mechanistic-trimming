8a47993524e306f41db187181b0c9c60b19c766e
magdalenarussell <magdalenarussell23@gmail.com>
2021-06-09 13:40:14 -0700

    organizing data compilation (motifs by subject)

--- scripts/data_compilation_functions.R
+++ scripts/data_compilation_functions.R
@@ -8,7 +8,7 @@ extract_subject_ID <- function(tcr_repertoire_file_path){
 }
 
 get_subject_motif_output_location <- function(){
-    output_location = file.path(OUTPUT_PATH, TRIM_TYPE, paste0(MOTIF_TYPE))
+    output_location = file.path(OUTPUT_PATH, TRIM_TYPE, paste0(MOTIF_TYPE, '_motif_', LEFT_NUC_MOTIF_COUNT, '_', RIGHT_NUC_MOTIF_COUNT))
     return(output_location)
 }
 
@@ -115,6 +115,7 @@ compile_motifs_for_subject <- function(file_path){
     together = merge(temp_data, gene_groups, by = GENE_NAME)
 
     motif_data = get_motifs(together, subject_id)
+
     fwrite(motif_data, file = file.path(output_location, paste0(subject_id, '.tsv')), sep = '\t')
 }
 
@@ -129,34 +130,4 @@ compile_all_motifs <- function(directory){
     stopImplicitCluster()
 }
 
-source(paste0('scripts/gene_weight_functions/', GENE_WEIGHT_TYPE, '.R'))
 
-aggregate_subject_data_by_trim_gene <- function(subject_data){
-    subject_data = calculate_subject_gene_weight(subject_data)
-    aggregated_subject = subject_data[, .N, by = .(trim_length, gene, motif, gene_type, gene_weight, subject, observed)]
-    setnames(aggregated_subject, 'N', 'count')
-    aggregated_subject[observed == FALSE, count := 0]
-    return(aggregated_subject)
-}
-
-split_motif_column_by_motif_position <- function(aggregated_subject_data){
-    split_data = aggregated_subject_data %>% separate('motif', c(paste0('motif_5end_pos', seq(LEFT_NUC_MOTIF_COUNT, 1)), paste0('motif_3end_pos', seq(1, RIGHT_NUC_MOTIF_COUNT))), sep = seq(1, LEFT_NUC_MOTIF_COUNT+RIGHT_NUC_MOTIF_COUNT-1))
-    
-    together = merge(aggregated_subject_data, split_data)
-    return(together)
-}
-
-# TODO move these functions to a different file, under the groups?
-aggregate_all_subject_data <- function(directory = get_subject_motif_output_location()){
-    #TODO add ability to look at only one subject or group
-    files = fs::dir_ls(path = directory)
-    registerDoParallel(cores=NCPU)
-    together = foreach(file = files, .combine=rbind) %dopar% {
-        file_data = fread(file)
-        print(paste(file))
-        aggregated_subject = aggregate_subject_data_by_trim_gene(file_data)
-        split_motif_column_by_motif_position(aggregated_subject) 
-    }
-    stopImplicitCluster()
-    return(together)
-}
--- scripts/model_group_functions/all_subjects.R
+++ scripts/model_group_functions/all_subjects.R
@@ -1,7 +1,10 @@
+GENE_WEIGHT_TYPE <<- 'p_gene_given_subject'
+
 calculate_subject_gene_weight <- function(subject_data){
     total_tcr = nrow(subject_data[observed == TRUE])
     gene_counts = subject_data[observed == TRUE, .N, by = .(gene)]
     gene_counts$gene_weight = gene_counts$N/total_tcr
+    gene_counts$gene_weight_type = GENE_WEIGHT_TYPE
     together = merge(subject_data, gene_counts[, c('gene', 'gene_weight')], by = 'gene')
     return(together)
 }
