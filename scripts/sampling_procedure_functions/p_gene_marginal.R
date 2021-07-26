484995fa98b88a116951774c75024ec0949a79fe
magdalenarussell <magdalenarussell23@gmail.com>
2021-07-26 14:41:53 -0700

    updating model procedure to new write-up plan

--- scripts/sampling_procedure_functions/p_gene_marginal.R
+++ scripts/sampling_procedure_functions/p_gene_marginal.R
@@ -11,6 +11,7 @@ calculate_subject_gene_weight <- function(compiled_data){
     subject_sum$gene_weight_type = GENE_WEIGHT_TYPE
     together = merge(compiled_data, subject_sum[, c('gene', 'p_gene', 'gene_weight_type')], by = 'gene')
     together$p_trim_given_gene = together$count/together$gene_count
-    together$weighted_observation = together$p_trim_given_gene * together$p_gene
+    p_subject = 1/total_subjects
+    together$weighted_observation = together$p_trim_given_gene * together$p_gene * p_subject
     return(together)
 }
