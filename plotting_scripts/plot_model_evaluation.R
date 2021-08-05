cacdcb0b3108befd6dcc10c00852a5afbf75b3a3
magdalenarussell <magdalenarussell23@gmail.com>
2021-08-05 14:03:21 -0700

    a few off-the-end protocol tweaks and adding model eval plots

--- plotting_scripts/plot_model_evaluation.R
+++ plotting_scripts/plot_model_evaluation.R
@@ -26,21 +26,19 @@ NCPU <<- as.numeric(args[3])
 GENE_NAME <<- paste0(substring(TRIM_TYPE, 1, 1), '_gene')
 stopifnot(GENE_NAME == 'v_gene')
 
-MODEL_GROUP <<- args[4]
+MODEL_GROUP <<- 'all_subjects'
 
-GENE_WEIGHT_TYPE <<- args[5]
+GENE_WEIGHT_TYPE <<- args[4]
 stopifnot(GENE_WEIGHT_TYPE %in% c('p_gene_given_subject', 'p_gene_marginal', 'raw_count'))
 
 # 5' motif nucleotide count
-LEFT_NUC_MOTIF_COUNT <<- as.numeric(args[6])
+LEFT_NUC_MOTIF_COUNT <<- as.numeric(args[5])
 # 3' motif nucleotide count
-RIGHT_NUC_MOTIF_COUNT <<- as.numeric(args[7])
+RIGHT_NUC_MOTIF_COUNT <<- as.numeric(args[6])
 
-source('scripts/data_compilation_functions.R')
-source('scripts/model_fitting_functions.R')
+source('scripts/model_evaluation_functions.R')
+source('plotting_scripts/plotting_functions.R')
 
-# Compile data for all subjects
-motif_data = aggregate_all_subject_data()
+plot_model_evaluation_scatter()
+plot_model_evaluation_heatmap()
 
-# Fit model, write predicted distribution and pwm files
-fit_model_by_group(motif_data)
--- scripts/evaluate_models.R
+++ scripts/evaluate_models.R
@@ -18,23 +18,23 @@ args = commandArgs(trailingOnly=TRUE)
 TRIM_TYPE <<- args[1]
 stopifnot(TRIM_TYPE == 'v_trim')
 
-MOTIF_TYPE <<- 'bounded'
+MOTIF_TYPE <<- args[2] 
 stopifnot(MOTIF_TYPE %in% c('bounded', 'unbounded'))
 
-NCPU <<- as.numeric(args[2])
+NCPU <<- as.numeric(args[3])
 
 GENE_NAME <<- paste0(substring(TRIM_TYPE, 1, 1), '_gene')
 stopifnot(GENE_NAME == 'v_gene')
 
 MODEL_GROUP <<- 'all_subjects'
 
-GENE_WEIGHT_TYPE <<- args[3]
+GENE_WEIGHT_TYPE <<- args[4]
 stopifnot(GENE_WEIGHT_TYPE %in% c('p_gene_given_subject', 'p_gene_marginal', 'raw_count'))
 
 # 5' motif nucleotide count
-LEFT_NUC_MOTIF_COUNT <<- as.numeric(args[4])
+LEFT_NUC_MOTIF_COUNT <<- as.numeric(args[5])
 # 3' motif nucleotide count
-RIGHT_NUC_MOTIF_COUNT <<- as.numeric(args[5])
+RIGHT_NUC_MOTIF_COUNT <<- as.numeric(args[6])
 
 source('scripts/data_compilation_functions.R')
 source('scripts/model_fitting_functions.R')
