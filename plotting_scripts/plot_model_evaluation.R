dfc5f6b81f6ec039633ee89f1b6e669dbb5cede6
magdalenarussell <magdalenarussell23@gmail.com>
2021-09-16 15:26:28 -0700

    fixing plotting functions

--- plotting_scripts/plot_model_evaluation.R
+++ plotting_scripts/plot_model_evaluation.R
@@ -39,12 +39,11 @@ UPPER_TRIM_BOUND <<- args[5]
 source('scripts/model_evaluation_functions.R')
 source('plotting_scripts/plotting_functions.R')
 
-plot_model_evaluation_heatmap(model_type_filter = 'motif')
-plot_model_evaluation_heatmap(model_type_filter = 'motif_distance')
-plot_model_evaluation_heatmap(model_type_filter = 'distance')
-plot_model_evaluation_heatmap()
-
-plot_model_evaluation_scatter(model_type_filter = 'motif')
-plot_model_evaluation_scatter(model_type_filter = 'motif_distance')
-plot_model_evaluation_scatter(model_type_filter = 'distance')
-plot_model_evaluation_scatter()
+for (type in c('per_gene', 'log_loss', 'per_gene_per_trim')){
+    for (model_type in c('motif', 'motif_distance', 'distance', 'motif_distance_terminal_gc_content', 'distance_terminal_gc_content', 'terminal_gc_content', 'motif_terminal_gc_content')){
+        plot_model_evaluation_heatmap(type, model_type_filter = model_type)
+        plot_model_evaluation_scatter(type, model_type_filter = model_type)
+    }
+    plot_model_evaluation_heatmap(type)
+    plot_model_evaluation_scatter(type)
+}
