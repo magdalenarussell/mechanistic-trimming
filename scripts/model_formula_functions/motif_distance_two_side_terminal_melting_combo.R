008449972cffacfd32c6de6713da1635ebdd1922
magdalenarussell <magdalenarussell23@gmail.com>
2021-12-14 15:48:15 -0800

    adding combined melting temperature calculation method; simple calculation for short sequences and nearest neighbors for long sequences

---
+++ scripts/model_formula_functions/motif_distance_two_side_terminal_melting_combo.R
@@ -0,0 +1,14 @@
+source(paste0(PROJECT_PATH, '/scripts/model_formula_functions/model_formula_specific_functions/two_side_terminal_melting.R'))
+
+get_model_formula <- function(){
+    motif_positions = get_positions() 
+    motif_positions_together = paste(motif_positions, collapse = ' + ')
+
+    formula = formula(paste0('cbind(weighted_observation, interaction(gene, subject)) ~ ', motif_positions_together, ' + as.factor(trim_length) + left_terminal_melting + right_terminal_melting'))
+    return(formula)
+}
+
+process_data_for_model_fit <- function(group_motif_data){
+    together = process_for_two_side_terminal_melting(group_motif_data, 'combo')
+    return(together)
+}
