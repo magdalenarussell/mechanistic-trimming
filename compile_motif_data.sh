5c76fbf4e667dc4054df735ff1aaac1cc5fa01ce
magdalenarussell <magdalenarussell23@gmail.com>
2022-01-06 10:02:58 -0800

    cleaning up scripts

--- compile_motif_data.sh
+++ compile_motif_data.sh
@@ -5,4 +5,13 @@ source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate exomotif2 
 set -eu
 
-Rscript $PWD/scripts/compile_motif_data.R $1 $2 $3 $4 $5 $6 $7 $8
+ANNOTATION_TYPE=$1
+TRIM_TYPE=$2
+PRODUCTIVITY=$3
+MOTIF_TYPE=$4
+NCPU=$5
+LEFT_MOTIF_COUNT=$6
+RIGHT_MOTIF_COUNT=$7
+UPPER_TRIM_BOUND=$8
+
+Rscript $PWD/scripts/compile_motif_data.R $ANNOTATION_TYPE $TRIM_TYPE $PRODUCTIVITY $MOTIF_TYPE $NCPU $LEFT_MOTIF_COUNT $RIGHT_MOTIF_COUNT $UPPER_TRIM_BOUND
