0ff4e26506c74f4aee9f39da7aa31dc82fd28c38
magdalenarussell <magdalenarussell23@gmail.com>
2022-01-20 10:39:01 -0800

    adding requirement for motif size given model type

--- scripts/model_formula_functions/distance.R
+++ scripts/model_formula_functions/distance.R
@@ -1,3 +1,5 @@
+stopifnot(LEFT_NUC_MOTIF_COUNT == 0 & RIGHT_NUC_MOTIF_COUNT == 0)
+
 get_model_formula <- function(){
     formula = formula(paste0('cbind(weighted_observation, interaction(gene, subject)) ~ as.factor(trim_length)'))
     return(formula)
