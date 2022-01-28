8b9b6549b75bad3ca7b5b3d253df6df06224cc97
magdalenarussell <magdalenarussell23@gmail.com>
2022-01-28 15:34:50 -0800

    refactoring model evaluation scripts to allow for different evaluation procedures

---
+++ scripts/model_evaluation_type_functions/aic.R
@@ -0,0 +1,19 @@
+HELD_OUT_FRACTION <<- NA 
+REPETITIONS <<- NA 
+WRITE_INTERMEDIATE_LOSS <<- NA 
+
+evaluate_loss <- function(motif_data) {
+    # fit model to the training data 
+    model = fit_model(motif_data)
+    parameter_count = length(model$coefficients) 
+    # adjust weighting to reflect actual p_gene_given_subject weight for log loss calculation
+    source(paste0('scripts/sampling_procedure_functions/p_gene_given_subject.R'), local = TRUE) 
+    motif_data = calculate_subject_gene_weight(motif_data)
+    # compute conditional logistic loss value for training data 
+    log_loss = calculate_cond_expected_log_loss(model, motif_data)
+    # compute aic 
+    aic = 2*log_loss + 2*parameter_count
+    return(list(loss = aic, model_parameter_count = parameter_count))
+}
+
+
