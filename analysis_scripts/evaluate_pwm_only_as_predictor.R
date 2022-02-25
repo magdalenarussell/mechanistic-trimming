3812ee96108acad21ed87fed27518e891bbc7253
magdalenarussell <magdalenarussell23@gmail.com>
2022-02-25 09:47:15 -0800

    updating input argument requirements

--- analysis_scripts/evaluate_pwm_only_as_predictor.R
+++ analysis_scripts/evaluate_pwm_only_as_predictor.R
@@ -50,7 +50,7 @@ UPPER_TRIM_BOUND <<- as.numeric(args[10])
 MODEL_TYPE <<- args[11]
 stopifnot(MODEL_TYPE %like% 'motif')
 
-if (grepl('_side_terminal_melting', MODEL_TYPE, fixed = TRUE)){
+if (grepl('_side_terminal', MODEL_TYPE, fixed = TRUE)){
     LEFT_SIDE_TERMINAL_MELT_LENGTH <<- as.numeric(args[12])
 } else {
     LEFT_SIDE_TERMINAL_MELT_LENGTH <<- NA
