de6524113c1efe24d5334d8d7551c36fc2dc49ca
magdalenarussell <magdalenarussell23@gmail.com>
2022-06-30 11:22:57 -0700

    adding procedure for calculating coeff significance

---
+++ scripts/evaluate_coeffs.R
@@ -0,0 +1,80 @@
+source('config/config.R')
+
+library(foreach)
+library(doParallel)
+library(tidyverse)
+library(data.table)
+setDTthreads(1)
+library(Biostrings)
+library(mclogit)
+library(matrixcalc)
+library(RhpcBLASctl)
+omp_set_num_threads(1)
+blas_set_num_threads(1)
+
+
+args = commandArgs(trailingOnly=TRUE)
+
+ANNOTATION_TYPE <<- args[1]
+stopifnot(ANNOTATION_TYPE %in% c('igor', 'parsimony'))
+
+TRIM_TYPE <<- args[2]
+trim_types = list.files(path = 'scripts/gene_specific_functions/')
+trim_types = str_sub(trim_types, end = -3)
+stopifnot(TRIM_TYPE %in% trim_types)
+
+PRODUCTIVITY <<- args[3]
+
+MOTIF_TYPE <<- args[4] 
+motif_types = list.files(path = 'scripts/motif_class_functions/')
+motif_types = str_sub(motif_types, end = -3)
+stopifnot(MOTIF_TYPE %in% motif_types)
+
+NCPU <<- as.numeric(args[5])
+
+GENE_NAME <<- paste0(substring(TRIM_TYPE, 1, 1), '_gene')
+
+# NOTE: This method is only applicable for models fit across all subjects!
+MODEL_GROUP <<- 'all_subjects'
+
+GENE_WEIGHT_TYPE <<- args[6]
+stopifnot(GENE_WEIGHT_TYPE %in% c('p_gene_given_subject', 'p_gene_marginal', 'raw_count', 'uniform'))
+
+# 5' motif nucleotide count
+LEFT_NUC_MOTIF_COUNT <<- as.numeric(args[7])
+# 3' motif nucleotide count
+RIGHT_NUC_MOTIF_COUNT <<- as.numeric(args[8])
+
+UPPER_TRIM_BOUND <<- as.numeric(args[9]) 
+
+MODEL_TYPE <<- args[10]
+
+if (grepl('_side_terminal', MODEL_TYPE, fixed = TRUE) | grepl('two-side-base-count', MODEL_TYPE, fixed = TRUE) | grepl('left-base-count', MODEL_TYPE, fixed = TRUE) | grepl('two-side-dinuc-count', MODEL_TYPE, fixed = TRUE)){
+    LEFT_SIDE_TERMINAL_MELT_LENGTH <<- as.numeric(args[11])
+} else {
+    LEFT_SIDE_TERMINAL_MELT_LENGTH <<- NA
+}
+
+source('scripts/data_compilation_functions.R')
+source('scripts/model_fitting_functions.R')
+source('plotting_scripts/individual_comparison_functions.R')
+
+# Compile data for all subjects
+motif_data = aggregate_all_subject_data()
+
+# bootstrap model fit
+bootstrap_result = cluster_bootstrap_model_fit(motif_data, iter = 1000)
+bootstrap_result$original_model_fit = FALSE
+
+# fit model with original dataset
+model = fit_model(motif_data)
+pwm_matrix = get_coeffiecient_matrix(motif_data, ref_base = 'A')
+pwm_dt = as.data.table(pwm_matrix)
+pwm_dt$base = rownames(pwm_matrix)
+coefs = format_model_coefficient_output(model, pwm_dt)
+coefs$original_model_fit = TRUE
+
+# write results
+results = get_coef_pvalues(bootstrap_result, coefs)
+filename =  get_model_bootstrap_file_name() 
+fwrite(results, filename, sep = '\t')
