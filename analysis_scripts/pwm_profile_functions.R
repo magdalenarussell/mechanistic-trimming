51205929751b1dfbb0f64ffdfafa3ec68672feaf
magdalenarussell <magdalenarussell23@gmail.com>
2022-09-26 11:25:49 -0700

    exploring model feature differences across datasets

--- analysis_scripts/pwm_profile_functions.R
+++ analysis_scripts/pwm_profile_functions.R
@@ -10,6 +10,51 @@ get_pwm_score <- function(pwm, motif, positions){
     return(score)
 }
 
+get_base_count_pwm_score <- function(pwm, row){
+    score = 0
+    for (count in c('left_base_count_GC', 'right_base_count_AT', 'right_base_count_GC')){
+        element = row[[count]]
+        element_score = (pwm[parameter == count]$coefficient) * as.numeric(element)
+        score = score + element_score
+    }
+    return(score)
+}
+
+get_all_base_count_pwm_score <- function(predicted_trims, pwm){
+    counts = c('left_base_count_GC', 'right_base_count_AT', 'right_base_count_GC')
+    condensed = unique(predicted_trims[,..counts])
+    condensed[, pwm_score := apply(condensed, 1, get_base_count_pwm_score, pwm = pwm)]
+    return(condensed)
+}
+
+get_all_pwm_score <- function(predicted_trims, pwm, per_gene_resid){
+    positions = get_positions()
+    cols = c('gene', 'trim_length', 'motif', positions)
+    condensed = unique(predicted_trims[,..cols]) 
+    # calculate pwm score by gene and trim length
+    condensed[, pwm_score := unlist(lapply(motif, function(x) as.numeric(get_pwm_score(pwm, x, positions))))]
+    if (!is.null(per_gene_resid)){
+        tog = merge(condensed, per_gene_resid)
+    } else {
+        tog = condensed
+    }
+    return(tog)
+}
+
+compare_weight_by_base_count <- function(predicted_trims){
+    avg = predicted_trims[, sum(weighted_observation), by = .(left_base_count_GC, right_base_count_GC, right_base_count_AT)]
+    setnames(avg, 'V1', 'total_weight')
+    return(avg)
+}
+
+compare_weight_by_motif <- function(predicted_trims){
+    avg = predicted_trims[, sum(weighted_observation), by = .(gene, trim_length, motif)]
+    setnames(avg, 'V1', 'total_weight')
+    avg2 = avg[, sum(total_weight), by = .(motif, gene)]
+    setnames(avg2, 'V1', 'total_weight_by_gene')
+    return(avg2)
+}
+
 determine_cluster_count_plot <- function(data){
     # Determine number of clusters
     data = data[, colSums(data != 0) > 0, with = FALSE]
@@ -68,4 +113,19 @@ get_pwm_prediction_residual_plot_file_path <- function() {
     return(path)
 }
 
-
+get_motif_frequency <- function(motif_data, model_performance_data, motif_of_interest){
+    positions = get_positions()
+    cols = c('gene', 'motif', 'trim_length', positions)
+    motifs = unique(motif_data[, ..cols])
+    tog = merge(motifs, model_performance_data)
+    tog[, freq := .N, by = gene]
+    tog[, motif_count := .N, by = .(motif, gene)]
+    tog = unique(tog[, -c('trim_length')])
+    tog[, motif_freq := motif_count/freq]
+    tog_subset = tog[motif %in% motif_of_interest]
+    other_subset = model_performance_data[!(gene %in% tog_subset$gene)]
+    other_subset[, motif := 'CTT/CGT']
+    other_subset[, motif_freq := 0]
+    tog_subset = rbind(tog_subset, other_subset, fill = TRUE)
+    return(tog_subset)
+}
