da1a7c1a53c9c98a0e0a6402f544c631afe0033e
magdalenarussell <magdalenarussell23@gmail.com>
2023-02-23 15:20:23 -0800

    adding subsampling experiment

---
+++ scripts/subsampling_experiment.R
@@ -0,0 +1,72 @@
+source('config/config.R')
+
+library(foreach)
+library(doParallel)
+library(tidyverse)
+library(data.table)
+setDTthreads(1)
+library(Biostrings)
+library(mclogit)
+library(matrixcalc)
+library(RhpcBLASctl)
+omp_set_num_threads(1)
+blas_set_num_threads(1)
+
+
+args = commandArgs(trailingOnly=TRUE)
+
+ANNOTATION_TYPE <<- args[1]
+stopifnot(ANNOTATION_TYPE %in% c('igor', 'parsimony'))
+
+TRIM_TYPE <<- args[2]
+trim_types = list.files(path = 'scripts/gene_specific_functions/')
+trim_types = str_sub(trim_types, end = -3)
+stopifnot(TRIM_TYPE %in% trim_types)
+
+PRODUCTIVITY <<- args[3]
+
+MOTIF_TYPE <<- args[4] 
+motif_types = list.files(path = 'scripts/motif_class_functions/')
+motif_types = str_sub(motif_types, end = -3)
+stopifnot(MOTIF_TYPE %in% motif_types)
+
+NCPU <<- as.numeric(args[5])
+
+GENE_NAME <<- paste0(substring(TRIM_TYPE, 1, 1), '_gene')
+
+# NOTE: This method is only applicable for models fit across all subjects!
+MODEL_GROUP <<- 'all_subjects'
+
+GENE_WEIGHT_TYPE <<- args[6]
+stopifnot(GENE_WEIGHT_TYPE %in% c('p_gene_given_subject', 'p_gene_marginal', 'raw_count', 'uniform'))
+
+# 5' motif nucleotide count
+LEFT_NUC_MOTIF_COUNT <<- as.numeric(args[7])
+# 3' motif nucleotide count
+RIGHT_NUC_MOTIF_COUNT <<- as.numeric(args[8])
+
+UPPER_TRIM_BOUND <<- as.numeric(args[9]) 
+
+MODEL_TYPE <<- args[10]
+
+PROP <<- as.numeric(args[11])
+
+if (grepl('_side_terminal', MODEL_TYPE, fixed = TRUE) | grepl('two-side-base-count', MODEL_TYPE, fixed = TRUE) | grepl('left-base-count', MODEL_TYPE, fixed = TRUE) | grepl('two-side-dinuc-count', MODEL_TYPE, fixed = TRUE)){
+    LEFT_SIDE_TERMINAL_MELT_LENGTH <<- as.numeric(args[12])
+} else {
+    LEFT_SIDE_TERMINAL_MELT_LENGTH <<- NA
+}
+
+source('scripts/data_compilation_functions.R')
+source('scripts/model_fitting_functions.R')
+source('plotting_scripts/individual_comparison_functions.R')
+
+# Compile data for all subjects
+motif_data = aggregate_all_subject_data()
+
+# subsample model fit
+results = subsample_model_fit(motif_data, formula = get_model_formula(), iter = 100, prop = PROP)
+
+# write results
+filename =  get_model_subsample_file_name(prop = PROP) 
+fwrite(results, filename, sep = '\t')
