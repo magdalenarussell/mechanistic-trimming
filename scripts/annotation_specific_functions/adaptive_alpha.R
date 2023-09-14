6e25fa83b8efbcdebf842ba45b359aa2c8945dd8
magdalenarussell <magdalenarussell23@gmail.com>
2023-09-14 10:37:44 -0700

    adding argument for annotation type

--- scripts/annotation_specific_functions/adaptive_alpha.R
+++ scripts/annotation_specific_functions/adaptive_alpha.R
@@ -1,14 +1,15 @@
 LOCUS <<- 'alpha'
+DATA_TYPE <<- 'adaptive'
 source(paste0(MOD_PROJECT_PATH, '/scripts/locus_specific_functions/', LOCUS, '.R'))
+source(paste0(MOD_PROJECT_PATH, '/scripts/data_type_functions/', DATA_TYPE, '.R'))
 
 extract_subject_ID <- function(tcr_repertoire_file_path){
-    file_name = str_split(tcr_repertoire_file_path, "/")[[1]][7]
+    file_name = str_split(tcr_repertoire_file_path, "/")[[1]][8]
     file_root_name = str_split(file_name, ".tsv")[[1]][1]
-    localID = str_split(file_root_name, "_")[[1]][1]
-    return(localID)
+    return(file_root_name)
 }
 
 get_whole_nucseqs <- function(){
-    whole_nucseq = fread(get(paste0('WHOLE_NUCSEQS_', ANNOTATION_TYPE)))[, -c('name')]
+    whole_nucseq = fread(get(paste0('WHOLE_NUCSEQS_', LOCUS)))[, -c('name')]
     return(whole_nucseq[, c('gene', 'sequence')])
 }
